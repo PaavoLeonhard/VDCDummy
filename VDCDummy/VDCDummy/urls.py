4d665c119587c045528806d0931adda449ada8ae
PaavoLeonhard <camps@campus.tu-berlin.de>
2018-10-16 16:20:44 +0200

    Added copyright to files

--- VDCDummy/VDCDummy/urls.py
+++ VDCDummy/VDCDummy/urls.py
@@ -12,6 +12,16 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+Copyright 2018 Information Systems Engineering, TU Berlin, Germany
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at    http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.This is being developed for the DITAS Project: https://www.ditas-project.eu/
+
 """
 from django.contrib import admin
 from django.urls import path
